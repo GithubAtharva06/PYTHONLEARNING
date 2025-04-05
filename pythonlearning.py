c725d649670c69c1b78daf077759b24bb10f4bfa
GithubAtharva06 <atharvachavan206@gmail.com>
2025-04-05 16:50:13 +0530

    first commit

---
+++ pythonlearning.py
@@ -0,0 +1,75 @@
+#house = 8
+#print(house)
+#credit = int(input("enter your credit score"))
+#if credit < house:
+#    print("you need to put down 20%")
+#elif credit >= house:
+#    print("you need to pay 10%")
+#    print("you have to pay 12000$")
+#else:
+#    print("you cant afford a house")
+
+
+#name = input("enter your name: ")
+#if len(name) < 3:
+#    print("the name must be ateast 3 characters long")
+#elif len(name) > 50:
+# print("the name must be maximum of 50 char long")
+#else:
+#    print("the name is good to go")
+
+
+#weight = int(input("weight: "))
+#unit = input("lbs or kg: ")
+#if unit.upper() == "L":
+#    converted = weight * 0.45
+#    print(f"you are {converted} kilos")
+#else:
+#    converted = weight / 0.45
+#    print(f"you are {converted} lbs")
+
+#guess = 1
+#while guess <= 5:
+#    print(guess)
+#    guess = guess + 1
+#print("done")
+
+
+
+#help = (input(">"))
+#while help == True:
+#    help.upper() == "HELP"
+#    print("start - to start the car")
+#    print("stop - to stop the car")
+#    print("quit - to exit")
+#    break
+#else:
+#    print("i dont understand that")
+#car = (input(">"))
+#while car.upper() == "START":
+#    print("car has started")
+#while car.upper() == "STOP":
+#        print("the car has stopped")
+#while car.upper() == "QUIT":
+#    print("game exited")
+#    break
+
+
+#command = ""
+#while command.lower() != "quit":
+#    command = input("> ")
+#    if command.lower() == "start":
+#        print("the car has started")
+#    elif command.lower() == "stop":
+#        print("the car is stopped")
+#    elif command.lower() == "help":
+#        print("start")
+#        print("stop")
+#        print("exit")
+#    elif command.lower() == "quit" or "exit":
+#        break
+#    else:
+#        print("i dont understand that")
+
+
+
